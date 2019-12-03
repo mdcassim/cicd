53c80b0b2acac35f47c81133cd94310e78a36482
Mohamed Cassim <mdcassimsait@gmail.com>
2019-12-03 16:35:22 +0530

    Update Jfrog_Upload_Script.sh

--- Scripts/Jfrog_Upload_Script.sh
+++ Scripts/Jfrog_Upload_Script.sh
@@ -1,1 +1,1 @@
-curl -uadmin:AP7GaTD6DrukYDezdWdmw8Zk4h3 -T /home/jenkins/node/workspace/Mule_CICD/target/*.jar "http://mdcassimsait.southindia.cloudapp.azure.com:8081/artifactory/generic-local/$buildNumber"
+curl -uadmin:AP7GaTD6DrukYDezdWdmw8Zk4h3 -T /home/jenkins/node/workspace/Mule_CICD/target/*.jar "http://mdcassimsait.southindia.cloudapp.azure.com:8081/artifactory/generic-local/$BUILD_NUMBER/*.jar"
